43744ca10c40d17a1e1bc713d673f61eeeba10fb
Will Hattingh <w.hattingh@nitecon.com>
2022-12-20 17:40:28 -0500

    add manager

---
+++ Plugins/AssetsBridge/Source/AssetsBridge/Private/BridgeManager.cpp
@@ -0,0 +1,8 @@
+// Copyright 2023 Nitecon Studios LLC. All rights reserved.
+
+
+#include "BridgeManager.h"
+
+UBridgeManager::UBridgeManager()
+{
+}
---
+++ Plugins/AssetsBridge/Source/AssetsBridge/Public/BridgeManager.h
@@ -0,0 +1,19 @@
+// Copyright 2023 Nitecon Studios LLC. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BridgeManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSETSBRIDGE_API UBridgeManager : public UObject
+{
+public:
+	GENERATED_BODY()
+
+	/** Generic constructor */
+	UBridgeManager();
+};
